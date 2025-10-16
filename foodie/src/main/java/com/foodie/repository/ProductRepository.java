12692d2cf1da26215467ffd1aad7013b3376cda9
abhishek.m <abhishek.m@EISWD022.EIS.LOCAL>
2025-10-16 18:37:34 +0530

    newone

--- foodie/src/main/java/com/foodie/repository/ProductRepository.java
+++ foodie/src/main/java/com/foodie/repository/ProductRepository.java
@@ -1,9 +1,14 @@
 package com.foodie.repository;
 
+import java.util.List;
+
 import org.springframework.data.jpa.repository.JpaRepository;
 
 import com.foodie.model.Product;
+import com.foodie.model.Vendor;
 
 public interface ProductRepository extends JpaRepository<Product, Long>{
+
+	 List<Product> findByVendor(Vendor vendor);
 
 }
